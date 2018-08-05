406221cd66018ac122cdcd0c74337eac03790c07
Arten013 <fukkn_kaz@yahoo.co.jp>
2018-08-06 07:34:06 +0900

    move dbconfig to other repo

--- jstatutree/graphtree/graph_lawdata.py
+++ jstatutree/graphtree/graph_lawdata.py
@@ -230,60 +230,7 @@ def split_list(alist, wanted_parts=1):
             for i in range(wanted_parts) ]
 
 import concurrent
-import configparser
 
-class DBConfig(object):
-    def __init__(self, path=None):
-        self.parser = configparser.ConfigParser()
-        if path is not None:
-            self.path = path
-            os.path.exists(self.path) and self.load()
-        self.section = self.parser['DEFAULT']
-
-    def change_section(self, name, create_if_missing=True):
-        if create_if_missing:
-            self.parser.add_section(name)
-        assert self.parser.has_section(name), 'There is no section named '+name+'.'
-        self.section = self.parser[name]
-
-    def set_path(self, path):
-        self.path = path
-
-    def load(self):
-        with open(self.path) as f:
-            self.parser.read_file(f)
-
-    def save(self):
-        os.makedirs(os.path.split(self.path)[0], exist_ok=True)
-        with open(self.path, 'w') as f:
-            self.parser.write(f, self)
-
-    def update(self):
-        assert self.path is not None, 'You must set path before reload database configure'
-        self.save()
-        self.load()
-
-    def set_logininfo(self, host='localhost', port='17474', usr='neo4j', pw='pass'):
-        self.section.update(
-                {
-                    'port': str(port),
-                    'usr': usr,
-                    'pw': pw,
-                    'host': host,
-                }
-            )
-
-    @property
-    def url(self):
-        return 'http://{host}:{port}'.format(host=self.section['host'], port=self.section['port'])
-
-    @property
-    def loginkey(self):
-        return {
-                    'username': self.section['usr'],
-                    'password': self.section['pw'],
-                    'url': self.url
-                }
 
 def register_directory(loginkey, levels, basepath, only_reiki=True, only_sentence=True, workers=multiprocessing.cpu_count()):
     path_lists = split_list(list(find_all_files(basepath, [".xml"])), workers)
